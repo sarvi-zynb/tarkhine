befd7689dd1f484711b66e510f71b2a94c4470ce
sarvi-zynb <sarvi.shr.79@gmail.com>
2024-08-03 20:15:08 +0330

    fix responsive menu card

--- src/components/MenuPage/card/index.jsx
+++ src/components/MenuPage/card/index.jsx
@@ -5,15 +5,14 @@ import { useShoppingCartContext } from "../../../contexts/ShoppingCart";
 const Card = ({ item }) => {
   const { addToCart } = useShoppingCartContext();
   return (
-      <div className='flex h-[100px] md:h-[130px] lg:h-[170px] border border-[#CBCBCB] rounded-md'>
-        <img src={item.image} className="w-1/3" />
-        <div className='grid p-1 md:p-3 mr-1 w-full'>
-          <div className='flex justify-between'>
-            <p className='text-[#353535] font-semibold text-sm sm:text-base lg:text-xl'>{item.name}</p>
-            <i className='iconsax text-[#717171] hidden md:flex text-base md:text-xl' icon-name='heart'></i>
-          </div>
-          <div className='flex justify-between items-center'>
-            <p className='text-[#353535] text-[10px] md:text-xs lg:text-sm'>{item.description}</p>
+    <div className='flex border border-[#CBCBCB] rounded-md'>
+      <img src={item.image} className='w-1/3' />
+      <div className='grid p-1 md:p-3 mr-1 w-full'>
+        <div className='flex justify-between'>
+          <p className='text-[#353535] font-semibold text-sm sm:text-base lg:text-xl'>
+            {item.name}
+          </p>
+          <div className='flex gap-10'>
             <div>
               {item.discount !== 0 && (
                 <div className='flex gap-3 items-center'>
@@ -25,20 +24,30 @@ const Card = ({ item }) => {
                   </div>
                 </div>
               )}
-              <p className='text-[#353535] text-[10px] lg:text-base mt-2'>
+              <p className='text-[#353535] text-[10px] lg:text-base'>
                 {item.price} تومان
               </p>
             </div>
-          </div>
-          <div className='flex justify-between items-center'>
-          <img src={starSmall} className='lg:hidden' />
-            <img src={star} className='max-lg:hidden h-5' />
-              <Button filled={true} onClick={() => addToCart(item.id)}>
-                افزودن به سبد خرید
-              </Button>
+            <i
+              className='iconsax text-[#717171] hidden md:flex text-base md:text-xl'
+              icon-name='heart'
+            ></i>
           </div>
         </div>
+        <div className='flex justify-between items-center'>
+          <p className='text-[#353535] text-[10px] md:text-xs lg:text-sm'>
+            {item.description}
+          </p>
+        </div>
+        <div className='flex justify-between items-end'>
+          <img src={starSmall} className='lg:hidden' />
+          <img src={star} className='max-lg:hidden h-5' />
+          <Button filled={true} onClick={() => addToCart(item.id)}>
+            افزودن به سبد خرید
+          </Button>
+        </div>
       </div>
+    </div>
   );
 };
 
--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -13,7 +13,7 @@ const Button = ({
         <>
           {filled ? (
             <button
-              className={`flex items-center justify-center text-white text-xs md:text-sm lg:text-base border border-[#417F56] bg-[#417F56] py-1 px-2 md:px-5 rounded-[4px] hover:bg-transparent hover:text-[#417F56] 
+              className={`flex items-center justify-center text-white text-[8px] sm:text-xs md:text-sm lg:text-base border border-[#417F56] bg-[#417F56] py-1 px-2 md:px-5 rounded-[4px] hover:bg-transparent hover:text-[#417F56] 
                 disabled:bg-[#cdcdcd] disabled:border-[#cdcdcd] disabled:hover:text-white disabled:hover:bg-[#cdcdcd] transition duration-500`}
               onClick={onClick}
               disabled={disabled}
@@ -23,7 +23,7 @@ const Button = ({
             </button>
           ) : (
             <button
-              className={`flex items-center justify-center text-white text-xs md:text-sm lg:text-base border border-white bg-transparent py-1 px-2 md:px-5 rounded-[4px] hover:bg-white hover:text-black
+              className={`flex items-center justify-center text-white text-[8px] sm:text-xs md:text-sm lg:text-base border border-white bg-transparent py-1 px-2 md:px-5 rounded-[4px] hover:bg-white hover:text-black
                  disabled:bg-[#cdcdcd] disabled:border-[#cdcdcd] disabled:hover:text-white disabled:hover:bg-[#cdcdcd] transition duration-500`}
               onClick={onClick}
               disabled={disabled}
@@ -37,7 +37,7 @@ const Button = ({
         <>
           {filled ? (
             <button
-              className={`flex items-center justify-center text-white text-xs md:text-sm lg:text-base border border-[#417F56] bg-[#417F56] py-1 px-2 md:px-5 rounded-[4px] hover:bg-transparent hover:text-[#417F56]
+              className={`flex items-center justify-center text-white text-[8px] sm:text-xs md:text-sm lg:text-base border border-[#417F56] bg-[#417F56] py-1 px-2 md:px-5 rounded-[4px] hover:bg-transparent hover:text-[#417F56]
                  disabled:bg-[#cdcdcd] disabled:border-[#cdcdcd] disabled:hover:bg-[#cdcdcd] disabled:hover:text-white transition duration-500 ${className}`}
               onClick={onClick}
               disabled={disabled}
@@ -47,7 +47,7 @@ const Button = ({
             </button>
           ) : (
             <button
-              className={`flex items-center justify-center text-[#417F56] text-xs md:text-sm lg:text-base border border-[#417F56] bg-white py-1.5 px-2 md:px-5 rounded-[4px] hover:bg-[#417F56] hover:text-white
+              className={`flex items-center justify-center text-[#417F56] text-[8px] sm:text-xs md:text-sm lg:text-base border border-[#417F56] bg-white py-1.5 px-2 md:px-5 rounded-[4px] hover:bg-[#417F56] hover:text-white
                  disabled:bg-[#cdcdcd] disabled:border-[#cdcdcd] disabled:hover:text-white disabled:hover:bg-[#cdcdcd] transition duration-500`}
               onClick={onClick}
               disabled={disabled}
