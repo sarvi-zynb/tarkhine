481aa106869280902e59f1902852e7f3c98ce6bd
sarvi-zynb <sarvi.shr.79@gmail.com>
2024-08-08 13:03:10 +0330

    change icon color

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -164,15 +164,15 @@ const Header = () => {
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 24 24'
                 fill='none'
-                className={`w-4 stroke-[${
-                  location.pathname == "/" ? "#417F56" : "#353535"
-                }] group-hover:stroke-[#417F56]`}
+                className='w-4'
               >
                 <path
                   d='M12 18v-3M10.07 2.82 3.14 8.37c-.78.62-1.28 1.93-1.11 2.91l1.33 7.96c.24 1.42 1.6 2.57 3.04 2.57h11.2c1.43 0 2.8-1.16 3.04-2.57l1.33-7.96c.16-.98-.34-2.29-1.11-2.91l-6.93-5.54c-1.07-.86-2.8-.86-3.86-.01Z'
+                  stroke={location.pathname == "/" ? "#417F56" : "#353535"}
                   stroke-width='1.5'
                   stroke-linecap='round'
                   stroke-linejoin='round'
+                  className='group-hover:stroke-[#417F56]'
                 ></path>
               </svg>
               <p className='text-xs md:text-base mr-1'>صفحه اصلی</p>
